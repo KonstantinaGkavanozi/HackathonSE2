9c794bcb43b3bc3ba1dadd7420fc7133ae087ca4
alexisbouletsis <alexisbouletsis@gmail.com>
2023-12-22 13:56:30 +0200

    Update userStory1.cy.js

--- cypress/e2e/PageObjects/userStory1.cy.js
+++ cypress/e2e/PageObjects/userStory1.cy.js
@@ -3,45 +3,29 @@ class AddProduct{
 
     chooseProduct(product){
         cy.get('.product-name')
-        .should('contain', product);
+          .should('contain', product);
     }
 
     increaseQuantity(product){
-
-        // cy.get('input[xpath="1"]').eq(1)
+        
         cy.contains('.product-name',product)
           .parent()
           .children('.product-name')
-          .should('contain',product);
+          .should('contain',product)
         };
-        // cy.get('.increment')
-        //   .parent()
-        //   .parent()
-        //   .children('.product-name')
-        //   .should('contain','Brocolli');
-    
-
-    // decreaseQuantity(){
-
-    // }
 
     addToCart(product){
-        // cy.get('.product-name')
-        //   .should('contain','Brocolli')
-        //   .parent()
-        //   .children('.product-action')
-        //   .get('button[type="button"]').click();
-        //   .within(() => {
-        //     cy.get('.product-action button[type="button"]').click();
-        // });
 
         cy.contains('.product-name',product)
           .parent()
-        //   .children('.product-action')
           .within(() => {
             cy.get('.product-action button[type="button"]').click();
             });
         };
+
+    openCart(){
+      cy.get('img[alt="Cart"]').click();
+    }
 }
 
 
